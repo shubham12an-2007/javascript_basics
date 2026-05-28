a22b00eb3b5a846e173ae2e259b7a3fdf6d27fec
Shubham Kumar Singh <shubham12an@gmail.com>
2026-05-28 13:13:13 +0000

    operations in js

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -27,3 +27,56 @@ let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
 
+
+// ************************* Operations ********************** //
+
+let value  = 3
+let negValue = -value 
+
+// console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2-2);
+// console.log(2 ** 3);
+// console.log(3 % 2);
+// console.log(2/3);
+
+
+let str1 = "hello"
+let str2 = " shubham"
+
+let str3 = str1 + str2 
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+// console.log(); 
+
+console.log(+true);  // 1
+console.log(+"");    // 0
+
+let num1 , num2 , num3 
+
+num1 = num2  = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter ++ ;
+console.log(gameCounter);
+
+
+// Perfix and Postfix
+let x = 3
+y = x ++  ;     // postfix (usage ke baad increment hogi value )
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+
+let a = 3;
+b = ++a ;         // prefic (usage se pehle increment hogi value )
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
