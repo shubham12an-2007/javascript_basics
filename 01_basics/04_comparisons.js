75b5f96da97c4d523c29d30628c93c450e282887
Shubham Kumar Singh <shubham12an@gmail.com>
2026-05-28 13:19:46 +0000

    comparison in js

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,27 @@
+// console.log(2 > 1);
+// console.log( 2 >= 1);
+// console.log( 2 < 1);
+// console.log( 2 <= 1);
+// console.log( 2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+// is tareeke ke conversion ko avoid krna  chaiye 
+console.log(null > 0);    // false 
+console.log(null == 0);
+console.log(null >= 0);   // idhar null ko 0 ke andar convert kardiya gya 
+
+console.log(undefined == 0); // false
+console.log(undefined >  0);   // false
+console.log(undefined < 0  );   // false 
+
+// ===  (strict check -> checks the data type also)
+console.log("2" === 2);   // false
+
+
+
+
+
