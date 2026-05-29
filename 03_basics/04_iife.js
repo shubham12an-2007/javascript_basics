4bc17cbc50c4bc6d2c5f2a5d6d06d2290fd766a3
Shubham Kumar Singh <shubham12an@gmail.com>
2026-05-29 07:39:04 +0000

    iife in js

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,2 +1,19 @@
 // Immediately Invoked Function Expressions (IIFE)
 
+// named iife 
+(function chai() {
+    console.log("DB CONNECTED");
+    
+}) () ;
+
+
+// second iife => an arrow function 
+
+// unnamed iife 
+((name) => {
+    console.log("DB CONNECTED TWO " + name);
+    
+}) ("shubham")
+
+
+
