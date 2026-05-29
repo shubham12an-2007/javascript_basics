203bdcc9e60c17efe28f7479c631360b56355e46
Shubham Kumar Singh <shubham12an@gmail.com>
2026-05-29 06:17:21 +0000

    objects in js part 2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,69 @@
+// const tinderUser = new Object()      // singleton object 
+const tinderUser = {}             // object literal 
+
+tinderUser.id = "123abc" ;
+tinderUser.name = "Sammy" ;
+tinderUser.isLoggedIn = false   ;
+
+// console.log(tinderUser); 
+
+const regularUser = {
+    email: "some@gmail.com" ,
+    fullname : {
+        userfullname : {
+            firstname : "hitesh" ,
+            lastname : "choudhary"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname); 
+
+const obj1 = {
+    1: "a" , 2:"b"
+}
+const obj2 = {3:"a" , 4: "b"} ;
+const obj4 = {5:"a" , 6:"b"} ;
+
+
+// to combine one or more objects into one object
+// const obj3 =  Object.assign({} , obj1 , obj2 , obj4)
+// console.log(obj3);
+
+
+// using spread operator
+const obj3 = {...obj1 , ...obj2 , ...obj4}
+console.log(obj3);
+
+// array of objects
+const users = [
+    {
+        id:1  ,
+        email: "sk@gmail.com" ,
+    } ,
+     {
+        id:2  ,
+        email: "eshh@gmail.com" ,
+    } ,
+     {
+        id:1  ,
+        email: "hitesh@gmail.com" ,
+    }
+
+]
+
+console.log(users[1].email)
+
+
+// to get every key of an object 
+
+console.log(Object.keys(tinderUser));   // array of keys 
+console.log(Object.values(tinderUser));    // array of values 
+
+console.log(Object.entries(tinderUser));   // array of key and value pairs 
+// [ [ 'id', '123abc' ], [ 'name', 'Sammy' ], [ 'isLoggedIn', false ] ]
+
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));  // true 
+
+
