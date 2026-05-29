dcbd5b748feb41fb68efb7a7a421a258d721469a
Shubham Kumar Singh <shubham12an@gmail.com>
2026-05-29 13:03:30 +0000

    while loops in js

---
+++ 05_iterations/two.js
@@ -0,0 +1,27 @@
+let index = 1;
+
+while(index < 5) {
+    console.log(`Value of index is ${index} ` );
+    index += 2 ;
+}
+
+// IN array (while loop )
+let myArr =  ["flash" , "batman" , "ironman" , "superman"]
+idx = 0;
+
+while(idx < myArr.length){
+    console.log(`Hero is : ${myArr[idx]}`);
+    idx += 1 ;
+}
+
+
+// do while loop
+
+let score = 11 ;
+do {
+    console.log(`Score is : ${score}`);
+    score += 1; 
+
+} while(score <= 10)
+
+console.log(score);   // 12 
