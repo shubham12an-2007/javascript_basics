afcc888df0a642c22f93882b665388738fa31d40
Shubham Kumar Singh <shubham12an@gmail.com>
2026-05-30 14:09:05 +0000

    project 1 solution code of color switcher

---
+++ 07_color_switcher/script.js
@@ -0,0 +1,24 @@
+// select all the buttons 
+const buttons = document.querySelectorAll(".button") ;
+const body  = document.querySelector("body") ;
+
+buttons.forEach(function (button) {
+    button.addEventListener('click' , function(e) {
+        
+        if(e.target.id === 'grey'){
+            body.style.backgroundColor = e.target.id ;
+        }
+
+        else if(e.target.id === "white"){
+            body.style.backgroundColor = e.target.id ;
+        } else if(e.target.id === "blue"){
+            body.style.backgroundColor = e.target.id ;
+        }
+        else if(e.target.id === "pink"){
+            body.style.backgroundColor = e.target.id ;
+        }
+        else{
+            body.style.backgroundColor = "yellow"
+        }
+    })
+})
